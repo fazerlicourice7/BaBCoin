34cd8c5c77a1c87112cbb05f931e6be8fb01f2e6
Vamshi Balanaga <vamshi.balanaga@berkeley.edu>
2019-10-30 19:00:56 -0700

    update

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 
 import App from './App';
 import Login from './components/login'
+import LandingPage form './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, serviceWorker.unregister());
+ReactDOM.render(<LandingPage />, serviceWorker.unregister());
