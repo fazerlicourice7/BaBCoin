2dd81c430ae9c42f3aed48e784b464a349b0febf
Nishan D'Souza <nishand@gmail.com>
2019-11-13 17:12:25 -0800

    /user and /rsvp

--- server/server.js
+++ server/server.js
@@ -16,15 +16,15 @@ let db = mongoose.connection;
 
 db.once('open', function() {
      console.log("Connected to database successfully!")
-     var query = User.find({'name': 'ilyrobert'}, function (err, results) {
-          if (results == null) {
-               console.log('yee');
-
-          } else {
-               console.log(results);
-               //res.send(results);
-          }
-     });
+     // var query = User.find({'name': 'ilyrobert'}, function (err, results) {
+     //      if (results == null) {
+     //           console.log('yee');
+     //
+     //      } else {
+     //           console.log(results);
+     //           //res.send(results);
+     //      }
+     // });
 });
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
@@ -38,45 +38,36 @@ server.post("/user", (req, res) => {
      var query = User.findOne({'name': user_name, 'email': user_email}, function (err, results) {
           if (results == null) {
                //Create new user:
-               let newUser = User({name: user_name, email: user_email});
+               let newUser = User({name: user_name, email: user_email, balance: 0, total_accrued: 0});
                newUser.save((err) => {
-
+                    res.sendStatus(400);
+                    return;
                });
 
+               res.status(200).send(newUser);
+
           } else {
-               console.log(typeof results);
-               res.send(results);
+               res.status(200).send(results);
           }
      });
 })
 
-server.get("/", (req, res) => {
-    res.send("hello there");
+server.post("/rsvp", (req, res) => { // req  -> has new BaBCoin balance for user
+     var user_email = req.body.email;
+     var newBabCoin = req.body.newBalance;
 
-});
+     const filter = {'email': user_email};
+     const update = {'balance':  newBabCoin};
 
-server.get("/:url", (req, res) => {
-    switch(req.params.url){
-        case "lolwhat" :
-            res.send("get rekt");
-            break;
-        case "never_say_never":
-            res.send("justin bieber");
-            break;
-        default:
-            res.send(`${req.params.url}`);
-            break;
-    }
-});
+     let doc = User.findOneAndUpdate(filter, update, {
+       new: true
+     });
 
-server.get("/lolwhat", (req, res) => {
-    res.send("get rekt");
-});
-
-server.post("/post", (req, res) => {
-    console.log('posting');
-    res.send(req.body);
-});
+     if (doc == null || doc.email == null) {
+          res.sendStatus(400);
+     }
+     res.status(200).send(results);
+})
 
 server.listen(port, () =>{
     console.log(`server listening at ${port}`);
