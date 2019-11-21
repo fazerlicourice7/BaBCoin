c2d8ab1be22480d20cbb6552641f842e61b7987a
Vamshi Balanaga <vamshi.balanaga@berkeley.edu>
2019-11-20 18:54:32 -0800

    add checkin endpiont

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 import * as constants from "../web/src/constants";
 import Web3 from "web3";
+
 const express = require('express');
 const server = express();
 const mongoose = require('mongoose');
@@ -23,7 +24,7 @@ const BabCoinContract = new web3.eth.Contract(
 
 var mongoDBURL = 'mongodb+srv://vamshi:vamshitest@babcoin-dyxc1.mongodb.net/test?retryWrites=true&w=majority';
 
-mongoose.connect(mongoDBURL, { useNewUrlParser: true });
+mongoose.connect(mongoDBURL, {useNewUrlParser: true});
 
 let db = mongoose.connection;
 
@@ -36,90 +37,111 @@ let db = mongoose.connection;
 // var babCoinContract = new web3.eth.Contract(ABI, '0x3d0a11636d9a3d5852127ea8ba2a77e52f2283b9');
 
 
-db.once('open', function() {
-     console.log("Connected to database successfully!")
+db.once('open', function () {
+    console.log("Connected to database successfully!")
 });
 
 db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 server.post("/user", (req, res) => {
-     //  req - username, user_email
-     // res - status code, user info
-     var user_name = req.body.name;
-     var user_email = req.body.email;
+    //  req - username, user_email
+    // res - status code, user info
+    var user_name = req.body.name;
+    var user_email = req.body.email;
 
-     var query = User.findOne({'name': user_name, 'email': user_email}, function (err, results) {
-          if (results == null) {
-               //Create new user:
-               let newUser = User({name: user_name, email: user_email, balance: 500, total_accrued: 500});
-               newUser.save((err) => {
-                    console.log(err);
-               });
-               if (newUser == null || newUser.name == null) {
-                    console.log("EERROOROROR");
-                    res.sendStatus(400);
-                    return;
-               }
-               res.status(200).send(newUser);
+    var query = User.findOne({'name': user_name, 'email': user_email}, function (err, results) {
+        if (results == null) {
+            //Create new user:
+            let newUser = User({name: user_name, email: user_email, balance: 500, total_accrued: 500});
+            newUser.save((err) => {
+                console.log(err);
+            });
+            if (newUser == null || newUser.name == null) {
+                console.log("EERROOROROR");
+                res.sendStatus(400);
+                return;
+            }
+            res.status(200).send(newUser);
 
-          } else {
-               res.status(200).send(results);
-          }
-     });
+        } else {
+            res.status(200).send(results);
+        }
+    });
 });
 
 server.post("/rsvp", (req, res) => { // req  -> has new BaBCoin balance for user
-     var user_email = req.body.email;
-     var delta = req.body.amount;
+    var user_email = req.body.email;
+    var delta = req.body.amount;
+    var going = req.body.going;
 
-     const filter = {'email': user_email};
+    const filter = {'email': user_email};
+    var _event, _user;
 
-     User.findOne(filter, function (err, user) {
-          if (user == null || user.balance < delta) {
-               res.sendStatus(400);
-               return;
-          }
-          user.balance = user.balance - delta;
-          user.save((err) => {
-               console.log(err);
-          });
-          res.status(200).send(user);
-     })
+    User.findOne(filter, function (err, user) {
+        if (user == null || user.balance < delta) {
+            res.sendStatus(400);
+            return;
+        }
+        if (going) {
+            user.balance = user.balance - delta;
+        } else {
+            user.balance = user.balance - delta / 2;
+        }
+        user.save((err) => {
+            console.log(err);
+        });
+        _user = user;
+    });
+
+    const eventFilter = {};
+    Event.findOne(eventFilter, (err, event) => {
+        if (event == null) {
+            res.sendStatus(400);
+            return;
+        }
+        event.save((err) => {
+            console.log(err);
+        });
+        event.rsvp_map.set(user_email, going);
+        _event = event;
+    });
+    res.status(200).send({_user, _event});
+
 });
 
 server.post("/checkin", (req, res) => {
-     var email = req.body.email;
-     var ical = req.body.ical;
+    var email = req.body.email;
+    var ical = req.body.ical;
 
-     const filter = {"iCalID": ical};
+    const filter = {"iCalID": ical};
 
-     Event.findOne(filter, (err, event) => {
-          if (event == null || !event.rsvp_map.contains(email)) {
-               res.sendStatus(400);
-               return;
-          }
-          event.save((err) => {
-               console.log(err);
-          });
-          event.attended.push(email);
-          res.status(200).send(event);
-     })
+    Event.findOne(filter, (err, event) => {
+        if (event == null || !event.rsvp_map.contains(email)) {
+            res.sendStatus(400);
+            return;
+        }
+        event.save((err) => {
+            console.log(err);
+        });
+        event.attended.push(email);
+        res.status(200).send(event);
+    });
 });
 
 server.post("/createEvent", (req, res) => {
-     var iCalID = req.body.calID;
-     var datetime = req.body.datetime; //format: 2016-11-14T20:30:00-08:00 - DATETtime-timezone
-     var name = req.body.name;
-     var description = req.body.description;
+    var iCalID = req.body.calID;
+    var datetime = req.body.datetime; //format: 2016-11-14T20:30:00-08:00 - DATETtime-timezone
+    var name = req.body.name;
+    var description = req.body.description;
 
-     let event = Event({iCalID: iCalID, datetime: datetime, name: name, description: description});
-     event.save((err) => {
-          console.log(err);
-     });
-     // res.status
+    let event = Event({iCalID: iCalID, datetime: datetime, name: name, description: description});
+    event.save((err) => {
+        console.log(err);
+    });
+    // res.status
 
 });
 
-server.listen(port, () =>{
+server.listen(port, () => {
     console.log(`server listening at ${port}`);
 });
