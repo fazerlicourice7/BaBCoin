0ee3ad936b9a7f7889070e3730fc12a398cf6973
Vamshi Balanaga <vamshi.balanaga@berkeley.edu>
2019-11-24 19:33:25 -0800

    ready for demo

--- web/src/constants.js
+++ web/src/constants.js
@@ -264,5 +264,5 @@ export const BABCoinABI = [
     "type": "function"
   }
 ];
-export const contractAddress = "0x445bc563425cb74ad633bA0DE4Da0279Bf56D6b9";
+export const contractAddress = "0xB75bf849216BB3fDe43687C247A1D908ec96CEeE";
 
